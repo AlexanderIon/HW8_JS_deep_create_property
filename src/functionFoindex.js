b72e6a4e52b69ba3b1aafc7993721a5a2b2094b2
AlexanderIon <tov.sandro@mail.ru>
2022-11-17 08:35:58 +0300

    Changes for functionFoIndex.js

--- src/functionFoindex.js
+++ src/functionFoindex.js
@@ -2,7 +2,7 @@ export default function order(someObj, someOrder) {
   const needList = [];
   const anyList = [];
 
-  for (const item in someObj) {
+  for (const item of Object.keys(someObj)) {
     const itemOflist = {};
     if (someOrder.includes(item)) {
       itemOflist.key = item;
@@ -23,7 +23,6 @@ export function optionAttack(someCharacter) {
   // console.log(someCharacter)
   const listOfSpecials = Object.create(someCharacter).special;
   for (const item in listOfSpecials) {
-    // console.log(Object.keys(listOfSpecials[item]))
     if (!Object.keys(listOfSpecials[item]).includes('description')) {
       listOfSpecials[item].description = 'Описание не доступно';
       // Object.defineProperty(listOfSpecials[item],'description ',{
